8427674045757ff65ee10d30ee852c4e5b43e7b7
fsluizvictor <fsluizvictor@gmail.com>
2020-05-11 15:36:35 -0300

    novo projeto

---
+++ src/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { createDrawerNavigator } from 'react-navigation'
+
+import Simples from './componentes/Simples'
+import ParImpar from './componentes/ParImpar'
+import { Inverter, MegaSena } from './componentes/multi'
+import Contador from './componentes/Contador'
+
+export default createDrawerNavigator({
+    Contador: {
+        screen: () => <Contador numeroInicial = {100}/>
+    },
+    MegaSena: {
+        screen: () => <MegaSena numeros={8} />,
+        navigationOptions: { title: 'Mega Sena' }
+    },
+    Inverter: {
+        screen: () => <Inverter texto='React Nativo' />
+    },
+    ParImpar: {
+        screen: () => <ParImpar numero={30} />,
+        navigationOptions: { title: 'Par Ímpar' }
+    },
+    Simples: {
+        screen: () => <Simples texto='Flexível' />
+    }
+
+}, { drawerWidth: 300 }) 
